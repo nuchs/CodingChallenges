e2a19a01c1424735931136866c6f58357bad6a16
nuchs <sjbrown@live.co.uk>
2025-10-22 14:08:22 +0100

    Manage file to be compressed

--- huff/main.go
+++ huff/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
@@ -10,5 +12,18 @@ func main() {
 	outfile := flag.String("o", "", "The encoded file")
 	flag.Parse()
 
+	file, err := os.Open(*infile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to open %s for reading: %s\n", *infile, err)
+		os.Exit(1)
+	}
+	defer closeFile(file)
+
 	fmt.Printf("Compressing %s and writing output to %s\n", *infile, *outfile)
+}
+
+func closeFile(file io.Closer) {
+	if err := file.Close(); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to close file: %s\n", err)
+	}
 }
