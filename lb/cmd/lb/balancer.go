dbf2807b67f61dbdaa08bd563bc3abf49f7d1f96
nuchs <sjbrown@live.co.uk>
2025-12-12 11:57:16 +0000

    carve out the load balancer into its own type and make it used he configured values

--- lb/cmd/lb/balancer.go
+++ lb/cmd/lb/balancer.go
@@ -1,61 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
 	"math/rand"
-	"net/http"
-	"strings"
 )
 
-func requestHandler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logRequest(r)
-
-		address := selectService()
-		resp, err := http.Get(address)
-		if err != nil {
-			msg := fmt.Sprintf("Failed to get response from backend: %s", err)
-			log.Println(msg)
-			http.Error(w, msg, http.StatusInternalServerError)
-			return
-		}
-		defer closeResponse(resp.Body)
-
-		text, err := io.ReadAll(resp.Body)
-		if err != nil {
-			msg := fmt.Sprintf("Could not read backend response: %s", err)
-			log.Println(msg)
-			http.Error(w, msg, http.StatusInternalServerError)
-			return
-		}
-
-		log.Printf("Response from server: %s %s\n", resp.Proto, resp.Status)
-		log.Printf("%s\n", string(text))
-
-		if _, err := w.Write(text); err != nil {
-			log.Printf("Failed to write response: %s\n", err)
-		}
-	})
+type Balancer struct {
+	urls []string
 }
 
-func logRequest(r *http.Request) {
-	log.Printf("Received request from %s\n", r.Host)
-	log.Printf("%s %s %s\n", r.Method, r.URL.Path, r.Proto)
-	for k, v := range r.Header {
-		log.Printf("%s: %s", k, strings.Join(v, ","))
-	}
-	log.Println()
-}
-
-func closeResponse(r io.Closer) {
-	if err := r.Close(); err != nil {
-		log.Printf("Failed to close request: %s\n", err)
+func NewBalancer(urls []string) *Balancer {
+	return &Balancer{
+		urls: urls,
 	}
 }
 
-func selectService() string {
-	service := rand.Intn(3) + 1
-	return fmt.Sprintf("http://be-%d", service)
+func (b *Balancer) selectService() (string, error) {
+	service := rand.Intn(len(b.urls))
+	return b.urls[service], nil
 }
--- lb/cmd/lb/server.go
+++ lb/cmd/lb/server.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -31,8 +32,9 @@ func configureLogger(out io.Writer) {
 }
 
 func newServer(cfg Config) *http.Server {
+	b := NewBalancer(cfg.Urls)
 	mux := http.NewServeMux()
-	mux.Handle("/", requestHandler())
+	mux.Handle("/", requestHandler(b))
 	webServer := &http.Server{
 		Addr:    fmt.Sprintf(":%d", cfg.Port),
 		Handler: mux,
@@ -54,5 +56,62 @@ func shutdownHandler(
 	if err := server.Shutdown(shutdownCtx); err != nil {
 		log.Printf("Failed to shutdown cleanly: %s\n", err)
 		os.Exit(1)
+	}
+}
+
+func requestHandler(b *Balancer) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logRequest(r)
+
+		address, err := b.selectService()
+		if err != nil {
+			msg := fmt.Sprintf("No backend services available: %s", err)
+			log.Println(msg)
+			http.Error(w, msg, http.StatusInternalServerError)
+		}
+
+		text, err := forwardRequest(address)
+		if err != nil {
+			msg := fmt.Sprintf("Failed to get response from backend: %s", err)
+			log.Println(msg)
+			http.Error(w, msg, http.StatusInternalServerError)
+			return
+		}
+
+		if _, err := w.Write(text); err != nil {
+			log.Printf("Failed to write response: %s\n", err)
+		}
+	})
+}
+
+func forwardRequest(address string) ([]byte, error) {
+	resp, err := http.Get(address)
+	if err != nil {
+		return nil, fmt.Errorf("getting response from backend: %w", err)
+	}
+	defer closeResponse(resp.Body)
+
+	text, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response from backend: %w", err)
+	}
+
+	log.Printf("Response from server: %s %s\n", resp.Proto, resp.Status)
+	log.Printf("%s\n", string(text))
+	return text, nil
+}
+
+func logRequest(r *http.Request) {
+	log.Printf("Received request from %s\n", r.Host)
+	log.Printf("%s %s %s\n", r.Method, r.URL.Path, r.Proto)
+	for k, v := range r.Header {
+		log.Printf("%s: %s", k, strings.Join(v, ","))
+	}
+	log.Println()
+}
+
+func closeResponse(r io.Closer) {
+	if err := r.Close(); err != nil {
+		log.Printf("Failed to close request: %s\n", err)
 	}
 }
